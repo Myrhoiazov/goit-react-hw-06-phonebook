c4e2da58a5881549cdbcdb8579e793bf21a8a68e
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-09-22 19:57:10 +0300

    add: start project

---
+++ src/redux/reducer.js
@@ -0,0 +1,20 @@
+const tasksInitialState = {
+  contacts: {
+    items: [],
+    filter: ''
+  }
+};
+
+
+
+export const tasksReducer = (state = tasksInitialState, action) => {
+  // Reducer code
+};
+
+  // const filtersInitialState = {
+  //   status: statusFilters.all,
+  // };
+
+  // export const filtersReducer = (state = filtersInitialState, action) => {
+  //   // Reducer code
+  // };
---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import { createAction, createReducer } from '@reduxjs/toolkit';
+// import { tasksReducer, filtersReducer } from './reducer';
+
+
+// export const store = configureStore({
+//   reducer: {
+//     tasks: tasksReducer,
+//     // filters: filtersReducer,
+//   }
+// });
